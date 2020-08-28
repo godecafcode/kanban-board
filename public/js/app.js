93b2a490f57ccb385676ae4d4f333c2cda91f2eb
godecafcode <pierreerik.io@gmail.com>
2020-08-28 16:22:44 +0200

    add interactive functions

--- public/js/app.js
+++ public/js/app.js
@@ -23,15 +23,6 @@ const ListCtrl = (function () {
     selectedList: null,
   }
 
-  const getListColor = function() {
-    const symLet = ['a','b','c','d','e','f',0,1,2,3,4,5,6,7,8,9];
-    let color = `#94`; // always begin with #94 for a consistant color design.
-    for (let i = 0; i < 4; i++) {
-      color += symLet[Math.floor(Math.random() * symLet.length)]
-    }
-    return color
-  }
-
   return {
     getListData: function() {
       return listData;
@@ -41,10 +32,24 @@ const ListCtrl = (function () {
       return listData.lists;
     },
 
-    constructList: function () {
+    // Get corresponding list color to each kanban
+    getListColor: function(parentList) {
+      // Get the last character from parentList.id and parse the number > id
+      parentList = parseInt(parentList.id.charAt(parentList.id.length - 1));
+      let color = '';
+      listData.lists.forEach(list => {
+        if(list.id === parentList) {
+          color = list.color;
+        }
+      });
+
+      return color;
+    },
+
+    constructList: function (listColor) {
       const ID = listData.lists.length;
       const name = `list-${ID}`
-      const color = getListColor();
+      const color = listColor;
       const list = new List(ID, name, color);
       listData.lists.push(list);
       console.log(listData.lists)
@@ -55,32 +60,34 @@ const ListCtrl = (function () {
 
 // Item Controller
 const ItemCtrl = (function () {
-  const Kanban = function (id, parent, text) {
+  const Kanban = function (id, parent, color, text) {
     this.id = id;
     this.parent = parent;
+    this.color = color;
     this.text = text;
   };
 
   const kanbanData = {
     kanbans: [
-      {id: 0, parent: 'item-0', text: 'Work'},
-      {id: 1, parent: 'item-1', text: 'Study'},
+      {id: 0, parent: 'kanban-0', text: 'Work'},
+      {id: 1, parent: 'kanban-1', text: 'Study'},
     ],
     selectedKanban: null,
   };
 
   return {
     getKanbanData: function () {
-      return data;
+      return kanbanData;
     },
 
     getKanbans: function () {
       return kanbanData.kanbans;
     },
 
-    constructKanban: function(kanbanText, listParentId) {
+    constructKanban: function(kanbanText, listParentId, correspColor) {
       const ID = kanbanData.kanbans.length;
-      const newKanban = new Kanban(ID, listParentId, kanbanText)
+      const color = correspColor;
+      const newKanban = new Kanban(ID, listParentId, color, kanbanText)
       kanbanData.kanbans.push(newKanban);
       console.log(newKanban)
       return newKanban;
@@ -97,7 +104,7 @@ const UICtrl = (function () {
     toggleEdit: '#toggle-edit',
     toggleRemove: '#toggle-remove',
     form: 'form',
-    kanbanText: '#item-text',
+    kanbanText: '#text',
     addKanban: '.add-kanban',
   };
 
@@ -116,7 +123,7 @@ const UICtrl = (function () {
           let parent = Number(kanban.parent.charAt(kanban.parent.length - 1));
           if(parent === list.id) {
             items += `
-            <div id="item-1" class="item misc_spread-md">
+            <div id="kanban-demo" class="item misc_spread-md">
             <div class="item-header">
               <span id="item-color" class="col"></span>
               <i id="reload" class="material-icons col">refresh</i>
@@ -129,7 +136,7 @@ const UICtrl = (function () {
           }
         })
         UIlists += `
-        <div id="list-1" class="list">
+        <div id="list-${list.id}" class="list">
         <div class="list-header misc_spread-sm">
           <h1>${list.name}</h1>
           <i class="material-icons add-kanban">add_circle_outline</i>
@@ -148,12 +155,12 @@ const UICtrl = (function () {
 
     UIconstructList: function(newList) {
       const list = document.createElement('div');
-      list.className = 'list';
       list.id = `list-${newList.id}`;
+      list.className = 'list';
       list.innerHTML = `
       <div class="list-header misc_spread-sm">
       <h1>List ${newList.id}</h1>
-      <i class="material-icons">add_circle_outline</i>
+      <i class="material-icons add-kanban">add_circle_outline</i>
     </div>
     <div class="list-body misc_spread-sm">
       <div id="list-body_${newList.id}" class="list-body_grid">
@@ -173,13 +180,14 @@ const UICtrl = (function () {
       document.querySelector(UISelectors.board).insertAdjacentElement('beforeend', list);
     },
 
-    UIconstructEmptyKanban: function(target) {
+    UIconstructEmptyKanban: function(target, correspColor) {
+      console.log(correspColor);
       const kanban = document.createElement('div');
-      kanban.className = 'item misc_spread-md';
       kanban.id = `kanban-empty`;
+      kanban.className = 'item misc_spread-md';
       kanban.innerHTML = `
       <div class="item-header">
-        <span id="item-color" class="col"></span>
+        <span id="item-color" class="col"" style="background: ${correspColor}"></span>
         <i id="reload" class="material-icons col">refresh</i>
       </div>
       <form class="form">
@@ -191,24 +199,23 @@ const UICtrl = (function () {
       document.querySelector(`#${target}`).insertAdjacentElement('beforeend', kanban);
     },
 
-    UIconstructKanban: function(demoParent, listParentId, kanbanObj) {
+    UIconstructKanban: function(demoParent, kanbanObj) {
       demoParent.remove();
-
       const kanban = document.createElement('div');
-      kanban.className = 'item misc_spread-md';
       kanban.id = `kanban-${kanbanObj.id}`;
+      kanban.className = 'item misc_spread-md';
       kanban.innerHTML = `
       <div class="item-header">
-        <span id="item-color" class="col"></span>
+        <span id="item-color" class="col" style="background: ${kanbanObj.color};"></span>
         <i id="reload" class="material-icons col">refresh</i>
       </div>
       <div class="item-text">
-        <p id="text">${kanbanObj.text}</p>
+        <p id="text" class="kanban-text">${kanbanObj.text}</p>
       </div>
       `;
 
      
-      document.querySelector(`#${listParentId}`).insertAdjacentElement('beforeend', kanban);
+      document.querySelector(`#${kanbanObj.parent}`).insertAdjacentElement('beforeend', kanban);
     }
   };
 })();
@@ -237,30 +244,56 @@ const App = (function (ListCtrl, ItemCtrl, UICtrl) {
       button.addEventListener('click', constructEmptyKanban);
     })
 
+    // Double click to change kanban text
+    const kanbanText = document.querySelectorAll(selectors.kanbanText);
+    kanbanText.forEach(text => {
+      text.addEventListener('dblclick', changeKanbanText);
+    });
+
+  };
+
+  const constructListColor = function() {
+    const symLet = ['a','b','c','d','e','f',0,1,2,3,4,5,6,7,8,9];
+    let color = `#94`; // always begin with #94 for a consistant color design.
+    for (let i = 0; i < 4; i++) {
+      color += symLet[Math.floor(Math.random() * symLet.length)]
+    }
+    return color
   };
 
   const createList = function () {
-    const newList = ListCtrl.constructList();
+    const color = constructListColor();
+    const newList = ListCtrl.constructList(color);
     UICtrl.UIconstructList(newList);
+    // re-assign event listeners to newly createn items.
+    loadEventListeners();
   };
 
   const constructEmptyKanban = function(e) {
     const target = e.target.parentNode.nextSibling.nextSibling.firstElementChild;
-    UICtrl.UIconstructEmptyKanban(target.id);
+    const listColor = ListCtrl.getListColor(e.target.parentNode.parentNode);
+    console.log(`list color: ${listColor}`) // Check if color is returned properly
+    UICtrl.UIconstructEmptyKanban(target.id, listColor);
+    // re-assign event listeners to newly createn items.
     loadEventListeners();
   }
 
   const createKanban = function (e) {
     const kanbanText = e.target.firstElementChild.value;
     const listParent = e.target.parentNode;
-    console.log(listParent)
     const listParentId = listParent.parentNode.id;
+    const correspList = e.target.parentNode.parentNode;
+    const correspColor = ListCtrl.getListColor(correspList);
     // Construct Kanban object
-    const kanban = ItemCtrl.constructKanban(kanbanText, listParentId);
+    const kanban = ItemCtrl.constructKanban(kanbanText, listParentId, correspColor);
     // Construct UI Kanban
-    UICtrl.UIconstructKanban(listParent, listParentId, kanban)
+    UICtrl.UIconstructKanban(listParent, kanban)
     e.preventDefault();
   };
+
+  const changeKanbanText = function(e) {
+    
+  }
 
   return {
     init: function () {
