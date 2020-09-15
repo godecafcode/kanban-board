55aae94a583829ebd30bdc8ebf0ba1423ca0244c
godecafcode <pierreerik.io@gmail.com>
2020-09-15 15:55:51 +0200

    deleted unused files

--- public/js/animateGrid.js
+++
@@ -1,7 +0,0 @@
-// const grid = document.querySelectorAll('list-body_grid');
-// grid.forEach(grid => {
-//   animateCSSGrid.wrapGrid(grid, {duration : 600});
-// })
-
-// grid.style.width = '500px'
-// forceGridAnimation()
